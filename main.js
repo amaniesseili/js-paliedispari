3d79e3f0d6ebac3a532c732085acc974ad92187c
amaniesseili <esseiliamani@hotmail.com>
2023-07-10 17:12:24 +0200

    js varialibi e condizioni che nn funzionano

--- main.js
+++ main.js
@@ -4,3 +4,27 @@
 //Sommiamo i due numeri -----------------------------------------------------------------> somma=scltaUtente + numroRandom
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)-----------> risoltato=(somma % 2 === 0)
 //Dichiariamo chi ha vinto.--------------------------------------------------------------> if pari/dispari + messaggio
+
+const btnGioca = document.querySelector(".btn-gioca");
+
+btnGioca.addEventListener( "click", function gioco(){
+    const numeroUtente = parseInt( document.getElementById(input-numero-utente).value) ; //numero utente 
+    const sceltaUtente= document.querySelector("input[name=scelta-utente]").value ;      //pari o dispari
+    
+    const numeroComputer = Math.floor(Math.random() * 5) + 1 ; // numero random per il computer
+    document.getElementById("uinput-numero-computer").innerHTML =  numeroComputer;
+
+    const somma = numeroUtente + numeroComputer; // la somma dei due numeri
+
+    let risultato = ( somma % 2 === 0 )   // per sapere se il risultato e pari o dispari 
+
+
+    if ( risultato === sceltaUtente) {
+        document.getElementById("messaggio").innerHTML =( "Complimeti, Hai vinto !!")
+    } else {
+        document.getElementById("messaggio").innerHTML =( "Ci dispiace, Hi perso!!")
+
+    }
+
+}
+)  
