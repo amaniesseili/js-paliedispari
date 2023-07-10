1a861d48b1d2bad12280fca49a299cb5b28ee3d1
amaniesseili <esseiliamani@hotmail.com>
2023-07-10 18:52:38 +0200

    ex.2 struttura html parola palindroma

--- main.js
+++ main.js
@@ -33,3 +33,9 @@ btnGioca.addEventListener( "click", function gioco(){
 
 }
 )  
+
+//  _____________________ Esercizio 2 ____________________
+//________________________ Palidroma ___________________
+//Chiedere all’utente di inserire una parola
+//Creare una funzione per capire se la parola inserita è palindroma
+//(una parola che si legge allo stesso modo anche da destra versi sinistra, ad esempio anna, otto)
