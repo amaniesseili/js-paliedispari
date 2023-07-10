9058d58424c6a1296720b8ca3c9452d583ff3d20
amaniesseili <esseiliamani@hotmail.com>
2023-07-10 16:06:21 +0200

    struttura html

---
+++ main.js
@@ -0,0 +1,6 @@
+// ------ --Pari e Dispari-------
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. -----------------------> sceltaUtente.value inserita in un 2 input html
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).----> numeroRnadom=math...
+//Sommiamo i due numeri -----------------------------------------------------------------> somma=scltaUtente + numroRandom
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)-----------> risoltato=(somma % 2 === 0)
+//Dichiariamo chi ha vinto.--------------------------------------------------------------> if pari/dispari + messaggio
